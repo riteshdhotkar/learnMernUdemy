cfb32e74227914a42a2be2f9a78851991869a2fe
ritesh <riteshdhotkar@hotmail.com>
2022-10-04 21:19:15 -0600

    User registration User route and middle setup

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {check, validationResult} = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
 
 //bring in user model. double dots takes you couple of levels up to the right folder 
 const User = require('../../models/User'); 
@@ -35,12 +40,45 @@ async (req,res)=> {
         let user = await User.findOne({ email });
 
         if(user){
-            res.status(400).json({ errors: [{msg:"User already exists"}]});
+            return res.status(400).json({ errors: [{msg:"User already exists"}]});
         }
 
+        const avatar = gravatar.url(email,{
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
 
+        user = new User({
+            name,
+            email,
+            gravatar,
+            password
+        })
 
-        res.send('User route');
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password,salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 360000},
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            }
+            );
+
+        
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server Error');
