81dcf924caf78d35834bc0ff51fb49e539a08579
ritesh <riteshdhotkar@hotmail.com>
2022-10-15 19:12:39 -0600

    Frontend Reigster WIP

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
  
 const App = () => (
@@ -11,6 +13,12 @@ const App = () => (
         <Routes>
           <Route exact path="/" element={<Landing />} />  
         </Routes>
+      <section className='container'>
+        <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </section>
     </Fragment>
   </Router>
 );
