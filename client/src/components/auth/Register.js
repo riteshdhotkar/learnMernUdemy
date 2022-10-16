59c84f523fd01fe108587313235226688cf543b2
ritesh <riteshdhotkar@hotmail.com>
2022-10-16 14:32:15 -0600

    Frontend Reigster interim finish

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, {Fragment, useState} from 'react'
+//import axios from 'axios'
 
 const Register = () => {
 
@@ -12,12 +13,32 @@ const Register = () => {
   const { name, email, password, password2} =formData;
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if(password!==password2) {
       console.log('Passwords do not match');
     }else {
-      console.log(formData);
+      console.log('SUCCESS')
+      /*const newUser = {
+        name,
+        email,
+        password
+      }
+      try {
+        const config = {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+
+        const body = JSON.stringify(newUser);
+
+        const res = await axios.post('/api/users', body, config);
+        console.log(res.data);
+      } catch (err) {
+        //console.error(err.response.data);
+
+      }*/
     }
   };
 
