495a383ef6dbe15ad7e1f157d889d7043f2c0077
ritesh <riteshdhotkar@hotmail.com>
2022-10-16 20:56:29 -0600

    Alert started but not completely working

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,31 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
  
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar/>
-        <Routes>
-          <Route exact path="/" element={<Landing />} />  
-        </Routes>
-      <section className='container'>
-        <Routes>
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-        </Routes>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Alert/>
+          <Routes>
+            <Route exact path="/" element={<Landing />} />  
+          </Routes>
+        <section className='container'>
+          <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
  
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, {Fragment, useState} from 'react'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 //import axios from 'axios'
 
-const Register = () => {
+const Register = ({setAlert}) => {
 
   const [formData, setFormData] = useState({
     name: '',
@@ -16,7 +20,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if(password!==password2) {
-      console.log('Passwords do not match');
+      setAlert('Passwords do not match', 'danger');
     }else {
       console.log('SUCCESS')
       /*const newUser = {
@@ -82,7 +86,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/Login">Sign In</Link>
       </p>
     </section>
 
@@ -90,4 +94,9 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, {setAlert}) (Register);
+
