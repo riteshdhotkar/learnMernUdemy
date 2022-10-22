8a91573f4c3e4f4f46870adc668d1b6270fd643e
Ritesh Dhotkar <ritesh@Riteshs-Mac-Studio.local>
2022-10-22 17:40:23 -0600

    43 logout and navbar links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_FAIL,
-    LOGIN_SUCCESS
+    LOGIN_SUCCESS,
+    LOGOUT
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -102,3 +103,10 @@ export const login = ( email, password) => async dispatch => {
         });
     }
 }
+
+//logout user. clear the profile 
+
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT});
+}
+
