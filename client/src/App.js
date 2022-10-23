6e3636afe6dc6f91b76ab129a6d8c2d70d4ac11d
Ritesh Dhotkar <ritesh@Riteshs-Mac-Studio.local>
2022-10-23 12:07:01 -0600

    44 protect route for dashboard complete and working with v6 routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,53 @@
-import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Alert from './components/layout/Alert';
+import React, { Fragment, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import Alert from './components/layout/Alert'
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './components/routing/PrivateRoute'
 //redux
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
+import { Provider } from 'react-redux'
+import store from './store'
+import { loadUser } from './actions/auth'
 
-import './App.css';
-import setAuthToken from './utils/setAuthToken';
+import './App.css'
+import setAuthToken from './utils/setAuthToken'
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
+  setAuthToken(localStorage.token)
 }
- 
-const App = () => { 
-  useEffect(()=>{
-    store.dispatch(loadUser());
-  }, []);
 
-  return(
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar/>
-        
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+
           <Routes>
-            <Route exact path="/" element={<Landing />} />  
+            <Route exact path='/' element={<Landing />} />
           </Routes>
-        <section className='container'>
-          <Alert/>
-          <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          </Routes>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-)};
- 
-export default App;
+          <section className='container'>
+            <Alert />
+            <Routes>
+              <Route exact path='/login' element={<Login />} />
+              <Route exact path='/register' element={<Register />} />
+              <Route
+                path='dashboard'
+                element={<PrivateRoute component={Dashboard} />}
+              />
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  )
+}
+
+export default App
